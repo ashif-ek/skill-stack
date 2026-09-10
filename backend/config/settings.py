2348803415700c41884bd3e674ef787b0b72844d
Ashif E.K <ashifek11@gmail.com>
2026-09-10 14:22:06 +0530

    fix: production settings, whitenoise, and gunicorn for railway

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-8k^%6o)(-fpxynhoa9(ghu4ct=_=$1a2$o9!2sha=z11737ri1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -64,6 +64,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
