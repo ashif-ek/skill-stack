c683595bc7b5f2046b9a33960345ee08f799d0f5
Ashif E.K <ashifek11@gmail.com>
2026-09-10 20:02:38 +0530

    fix: configure production static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,9 +130,14 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
